94bf7dd806ecd998f08b6b4694337027a78e9718
feng.hu <feng.hu@bioon.com>
2018-08-14 18:02:54 +0800

    add a new console repository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/14
+ * Time: 17:58
+ */
+
+namespace App\Repositories;
+
+
+abstract class  BaseRepository
+{
+    abstract function model();
+}
---
+++ app/Repositories/TestRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+ namespace App\Repositories;
+
+
+ class TestRepository extends BaseRepository
+ {
+
+     /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        //use model
+    }
+
+ }
